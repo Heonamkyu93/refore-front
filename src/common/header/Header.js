cd95fa9603ac73e070b43fb34346399bad6dd73b
Heonamkyu93 <heonamkyu930527@gmail.com>
2024-01-23 22:17:00 +0900

    헤더 풋터 생성

---
+++ src/common/header/Header.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styles from './Header.module.css'; // CSS 모듈 임포트
+
+const Header = () => {
+    return (
+        <header className={styles.Header}>
+            <Link to="/" className={styles.Logo}>
+                <img src="/path/to/logo.png" alt="Logo" />
+            </Link>
+            <nav className={styles.Navigation}>
+                <Link to="/link1" className={styles.NavLink}>링크 1</Link>
+                <Link to="/link2" className={styles.NavLink}>링크 2</Link>
+                <Link to="/link3" className={styles.NavLink}>링크 3</Link>
+                <Link to="/link4" className={styles.NavLink}>링크 4</Link>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;
