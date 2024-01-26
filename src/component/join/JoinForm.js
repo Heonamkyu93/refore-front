6a43e653fa71914d18f25c597f9880d0b7f21f48
Heonamkyu93 <heonamkyu930527@gmail.com>
2024-01-26 21:15:15 +0900

    joinForm 클라이언트 유효성검사

--- src/component/join/JoinForm.js
+++ src/component/join/JoinForm.js
@@ -1,49 +1,57 @@
 import React, { useState,useEffect } from 'react';
+import styles from './JoinForm.module.css';
 const JoinForm = () => {
-    const [webSocket, setWebSocket] = useState(null);
-    useEffect(() => {
-        // 웹소켓 연결 생성
-        const userToken = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhY2Nlc3MiLCJleHAiOjE3MDU5NzU0MzYsImlkIjoyLCJlbWFpbCI6ImV4YW1wbGUyQGVtYWlsLmNvbSJ9.8g57o1x0Zn55dfWxJxUVJeRftrb_tykB3VFLnggjEwRoBvlC9jx6X30FEYWHJs6GJ43rQnxYrdoIFaUIkGrfKw';
-        const refreshToken = 'test';
-        const ws = new WebSocket('ws://localhost:8888/ws/chat');
-        ws.onopen = () => {
+  const [formData,setFormData] = useState({
+    memberEmail:"",
+    memberPassword:"",
+    memberName:"",
+    memberNickname:"",
+    phoneNumber:"",
+    confirmPassword:"",
+  });
+  
+    const changeValue =(e) =>{
+      const {name , value} = e.target;
+      setFormData({...formData,[name]:value});
+    };
 
-          ws.send(JSON.stringify({ accessToken: userToken, refreshToken: refreshToken }));
 
-
-
-          console.log('Connected to the WebSocket server');
-          // 연결이 성공적으로 이루어지면 필요한 로직을 여기에 작성합니다.
-        };
-    
-        ws.onmessage = (event) => {
-          // 서버로부터 메시지를 받았을 때의 처리 로직을 여기에 작성합니다.
-          console.log('Message from server:', event.data);
-        };
-    
-        ws.onerror = (error) => {
-          // 오류 처리 로직을 여기에 작성합니다.
-          console.error('WebSocket error:', error);
-        };
-    
-        ws.onclose = () => {
-          console.log('Disconnected from the WebSocket server');
-          // 연결이 종료되면 필요한 로직을 여기에 작성합니다.
-        };
-    
-        // WebSocket 객체를 상태로 설정
-        setWebSocket(ws);
-    
-        // 컴포넌트가 언마운트될 때 WebSocket 연결을 종료합니다.
-        return () => {
-          ws.close();
-        };
-      }, []);
     return (
-        <div>
-            <h1>join </h1>          
+        <div className={styles.joinContainer}>
+           <div className={styles.joinFormContainer}>
+              <form>
+              <div className={styles.joinInputDiv}>
+              <span>이메일</span><input type='email' maxLength={50} required onChange={changeValue} name='memberEmail'></input>
+              <button type='button'>중복확인</button>
+              </div>
+              <div className={styles.joinInputDiv}>
+                <span>닉네임</span>
+                <input type='text' maxLength={15} required onChange={changeValue} name='memberNickname'></input>
+                <button type='button'>중복확인</button>
+              </div>
+              <div className={styles.joinInputDiv}>
+                <span>이름</span>
+                <input type='text' maxLength={10} required onChange={changeValue} name='memberName'></input>
+              </div>
+              <div className={styles.joinInputDiv}>
+                <span>비밀번호</span>
+                <input type='password' maxLength={10} required onChange={changeValue} name='memberPassword'></input>
+              </div>
+              <div className={styles.joinInputDiv}>
+                <span>비밀번호 확인</span>
+                <input type='password' maxLength={10} required onChange={changeValue} name='confirmPassword'></input>
+              </div>
+              <div className={styles.joinInputDiv}>
+                <span>전화번호</span>
+                <input type='text' maxLength={11} required onChange={changeValue} name='phoneNumber'></input>
+              </div>
+              <div className={styles.joinInputDiv}>
+              <input type="submit" value="회원가입"></input>
+              </div>
+              </form>
+           </div>
         </div>
     );
 };
 
-export default JoinForm;            <h1>join </h1>
+export default JoinForm;           
