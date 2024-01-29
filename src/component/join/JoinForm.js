4c006bc0b2a4120112caf157898e6d9cec8802ad
Heonamkyu93 <heonamkyu930527@gmail.com>
2024-01-29 20:52:47 +0900

    join 검증 로직 수정

--- src/component/join/JoinForm.js
+++ src/component/join/JoinForm.js
@@ -6,6 +6,11 @@ const serverPort = process.env.REACT_APP_SPRING_BOOT_PORT;
 
 
 const JoinForm = () => {
+  const [isEmailChecked, setIsEmailChecked] = useState(false);
+  const [isPhoneChecked, setIsPhoneChecked] = useState(false);
+  const [isNicknameChecked, setIsNicknameChecked] = useState(false);
+
+
   const [formData,setFormData] = useState({
     memberEmail:"",
     memberPassword:"",
@@ -16,14 +21,53 @@ const JoinForm = () => {
   });
   
     const changeValue =(e) =>{
-      const test= serverIp;
-      console.log(test);
+      const inputId=e.target.id;
+      if(inputId==='memberEmail'){
+      setIsEmailChecked(false);
+      }else if(inputId==='phoneNumber'){
+        setIsPhoneChecked(false);
+      }else if(inputId==='nickname'){
+        setIsNicknameChecked(false);
+      }
       const {name , value} = e.target;
       setFormData({...formData,[name]:value});
     };
+
+
+
     const sendData = (e) => {
       e.preventDefault();
-     
+      let inputName=document.getElementById('memberName').value;
+      let inputPwd=document.getElementById('memberPassword').value;
+      let inputConfirmPwd=document.getElementById('confirmPassword').value;
+
+
+      if(inputName.length<2 || inputName > 10){
+        alert('이름은 2자리 이상 10자리 이하여야 합니다.');
+        return;
+      }else if(inputPwd.length <10 || inputPwd >30){
+        alert('비밀번호는 10자리 이상 30자리 이하여야 합니다.');
+        return;
+      }else if(!inputPwd===inputConfirmPwd){
+        alert('비밀번호와 비밀번호 확인은 값이 같아야 합니다.');
+        return;
+      }
+
+
+
+
+      if (!isEmailChecked) {
+        alert('이메일 중복 확인을 해주세요.');
+        return;
+      }else if(!isPhoneChecked){
+        alert('전화번호 중복 확인을 해주세요.');
+        return;
+      }else if (!isNicknameChecked){
+        alert('닉네임 중복 확인을 해주세요.');
+        return;
+      }
+
+
       axios.post(`http://${serverIp}:${serverPort}/join`,formData)
       .then(response => {
       })
@@ -39,11 +83,28 @@ const JoinForm = () => {
       let inputElement;
       let url; 
       if (type === 'email') {
+       let confirmEmail=document.getElementById('memberEmail');
+       if (confirmEmail.validity.valid) {
         inputElement = document.getElementById('memberEmail').value;
         url = `http://${serverIp}:${serverPort}/emailDuplicatedCheck?value=${inputElement}`;
+       }else{
+        alert('유효하지 않은 이메일 형식입니다.');
+        return;
+       }
       } else if (type === 'nickname') {
         inputElement = document.getElementById('nickname').value;
+        if (inputElement.length < 2 || inputElement.length > 20) {  // 2미만 20초과
+          alert('닉네임은 2자리 이상 20자리 이하여야 합니다.');
+          return;
+        }
         url = `http://${serverIp}:${serverPort}/nicknameDuplicatedCheck?value=${inputElement}`;
+      }else if(type === 'phoneNumber'){
+        inputElement = document.getElementById('nickname').value;
+        if (inputElement.length < 10 || inputElement.length > 11) {  // 10미만 11초과
+          alert('전화번호는 10자리 이상 11자리 이하여야 합니다.');
+          return;
+        }
+        url = `http://${serverIp}:${serverPort}/phoneNumberDuplicatedCheck?value=${inputElement}`;
       }
       axios.get(url)
       .then(response => {
@@ -65,26 +126,28 @@ const JoinForm = () => {
               <button type='button' onClick={duplicatedCheck} data-type='email'>중복확인</button>
               </div>
               <div className={styles.joinInputDiv}>
+                <span>전화번호</span>
+                <input type='text' maxLength={11} required onChange={changeValue} name='phoneNumber'></input>
+                <button type='button' onClick={duplicatedCheck} data-type='phoneNumber'>중복확인</button>
+              </div>
+              <div className={styles.joinInputDiv}>
                 <span>닉네임</span>
                 <input type='text' maxLength={15} required onChange={changeValue} name='nickname' id='nickname'></input>
                 <button type='button' onClick={duplicatedCheck} data-type='nickname'>중복확인</button>
               </div>
               <div className={styles.joinInputDiv}>
                 <span>이름</span>
-                <input type='text' maxLength={10} required onChange={changeValue} name='memberName'></input>
+                <input type='text' maxLength={10} required onChange={changeValue} name='memberName' id='memberName' ></input>
               </div>
               <div className={styles.joinInputDiv}>
                 <span>비밀번호</span>
-                <input type='password' maxLength={10} required onChange={changeValue} name='memberPassword'></input>
+                <input type='password' maxLength={10} required onChange={changeValue} name='memberPassword' id='memberPassword'></input>
               </div>
               <div className={styles.joinInputDiv}>
                 <span>비밀번호 확인</span>
-                <input type='password' maxLength={10} required onChange={changeValue} name='confirmPassword'></input>
+                <input type='password' maxLength={10} required onChange={changeValue} name='confirmPassword' id='confirmPassword'></input>
               </div>
-              <div className={styles.joinInputDiv}>
-                <span>전화번호</span>
-                <input type='text' maxLength={11} required onChange={changeValue} name='phoneNumber'></input>
-              </div>
+             
               <div className={styles.joinInputDiv}>
               <input type="submit" value="회원가입" ></input>
               </div>
