b10bd7eb3730d45c901142ff1db73503d88bbe89
Heonamkyu93 <heonamkyu930527@gmail.com>
2024-02-06 22:58:47 +0900

    헤더 로그아웃 수정

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,36 +1,36 @@
-import { Link,NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
-import { useState,React } from 'react';
+import { useState, React } from 'react';
 import { useNavigate } from 'react-router-dom';
+
 const Header = () => {
     const userInfo = JSON.parse(sessionStorage.getItem('userInfo'));
     const navigate = useNavigate();
+
     const handleLogout = () => {
         sessionStorage.removeItem('userInfo');
         sessionStorage.removeItem('jwt');
         sessionStorage.removeItem('refreshToken');
+        navigate("/");
     }
-
 
     return (
         <header className={styles.Header}>
-            <NavLink to="/" className={styles.Logo}>
-                <img  src="/img/react.png" alt="Logo" />
-            </NavLink>
+            <Link to="/" className={styles.Logo}>
+                <img src="/img/react.png" alt="Logo" />
+            </Link>
             <nav className={styles.Navigation}>
-                {userInfo ? (<>
-                   
-                     <NavLink to="/mypage" className={styles.NavLink}>마이페이지</NavLink>
-                     <NavLink  onClick={handleLogout} className={styles.NavLink}>로그아웃</NavLink>
-                </>):(<>
-                
-                    <NavLink to="/join" className={styles.NavLink}>회원가입</NavLink>
-                    <NavLink to="/login" className={styles.NavLink}>로그인</NavLink>
-                
-                </>)}
-              
-            
-               
+                {userInfo ? (
+                    <>
+                        <Link to="/mypage" className={styles.NavLink}>마이페이지</Link>
+                        <button onClick={handleLogout} className={styles.NavLink}>로그아웃</button>
+                    </>
+                ) : (
+                    <>
+                        <Link to="/join" className={styles.NavLink}>회원가입</Link>
+                        <Link to="/login" className={styles.NavLink}>로그인</Link>
+                    </>
+                )}
             </nav>
         </header>
     );
