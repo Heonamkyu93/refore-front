9edf014b3f81abf3fe109b28f4bfb8ce39d3e360
Heonamkyu93 <heonamkyu930527@gmail.com>
2024-02-09 21:37:12 +0900

    이미지 업로드 css 수정

--- src/component/upimg/Upimg.js
+++ src/component/upimg/Upimg.js
@@ -1,47 +1,54 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import styles from './Upimg.module.css'; // CSS 모듈 import
+
 const serverIp = process.env.REACT_APP_FASTAPI_IP;
 const serverPort = process.env.REACT_APP_FASTAPI_PORT;
+
 const Upimg = () => {
-   
     const [selectedFile, setSelectedFile] = useState(null);
+    const [preview, setPreview] = useState('');
+    const [fileName, setFileName] = useState(''); // 파일 이름 상태 추가
 
     const handleFileChange = (event) => {
-      const file = event.target.files[0];
-      // 파일이 이미지 형식인지 검사합니다.
-      if (file && file.type.startsWith('image/')) {
-        setSelectedFile(file);
-      } else {
-        alert('이미지 파일이 아닙니다.');
-      }
-    };
-  
-    const handleUpload = async () => {
-      if (selectedFile) {
-        const formData = new FormData();
-        formData.append('file', selectedFile);
-  
-        try {
-          const response = await axios.post(`http://${serverIp}:${serverPort}/in/upimg`, formData, {
-            headers: {
-              'Content-Type': 'multipart/form-data',
-            },
-          });
-          console.log('서버 응답:', response.data);
-          // 업로드 성공 후 처리
-        } catch (error) {
-          console.error('업로드 실패:', error);
-          // 업로드 실패 처리
+        const file = event.target.files[0];
+        if (file && file.type.startsWith('image/')) {
+            setSelectedFile(file);
+            setPreview(URL.createObjectURL(file));
+            setFileName(file.name); // 파일 이름 설정
+        } else {
+            alert('이미지 파일이 아닙니다.');
         }
-      }
     };
-  
+
+    const handleUpload = async () => {
+        if (selectedFile) {
+            const formData = new FormData();
+            formData.append('file', selectedFile);
+
+            try {
+                const response = await axios.post(`http://${serverIp}:${serverPort}/in/upimg`, formData, {
+                    headers: {
+                        'Content-Type': 'multipart/form-data',
+                    },
+                });
+                console.log('서버 응답:', response.data);
+            } catch (error) {
+                console.error('업로드 실패:', error);
+            }
+        }
+    };
+
     return (
-      <div>
-        <input type="file" onChange={handleFileChange} />
-        <button onClick={handleUpload}>업로드</button>
-      </div>
+        <div className={styles.container}>
+            <div className={styles.uploadControl}>
+                <input type="file" id="fileInput" className={styles.fileInput} onChange={handleFileChange} />
+                <label htmlFor="fileInput" className={styles.fileInputLabel}>{fileName || '파일 선택'}</label>
+                <button onClick={handleUpload} className={styles.uploadButton}>업로드</button>
+            </div>
+            {preview && <img src={preview} alt="Preview" className={styles.imagePreview} />}
+        </div>
     );
-  }
-  
-export default Upimg;<h1>이미지</h1>
+}
+
+export default Upimg;
