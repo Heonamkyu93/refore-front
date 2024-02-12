ab769d2e4752923f12783a7572d7bbcb9e361481
Heonamkyu93 <heonamkyu930527@gmail.com>
2024-02-12 20:05:45 +0900

    동물상 사진 유효성검증후 이미지파일형식 변경

--- src/component/upimg/Upimg.js
+++ src/component/upimg/Upimg.js
@@ -13,13 +13,32 @@ const Upimg = () => {
     const handleFileChange = (event) => {
         const file = event.target.files[0];
         if (file && file.type.startsWith('image/')) {
-            setSelectedFile(file);
-            setPreview(URL.createObjectURL(file));
-            setFileName(file.name); // 파일 이름 설정
+            const reader = new FileReader();
+            reader.onload = (e) => {
+                const img = document.createElement("img");
+                img.onload = () => {
+                    const canvas = document.createElement("canvas");
+                    canvas.width = img.width;
+                    canvas.height = img.height;
+    
+                    const ctx = canvas.getContext("2d");
+                    ctx.drawImage(img, 0, 0);
+    
+                    canvas.toBlob((blob) => {
+                        const newFile = new File([blob], "converted.jpg", { type: "image/jpeg", lastModified: Date.now() });
+                        setSelectedFile(newFile);
+                        setPreview(URL.createObjectURL(newFile));
+                        setFileName(newFile.name); // 파일 이름 설정
+                    }, 'image/jpeg', 1); // 마지막 인자는 JPG 품질 설정 (0 ~ 1)
+                };
+                img.src = e.target.result;
+            };
+            reader.readAsDataURL(file);
         } else {
             alert('이미지 파일이 아닙니다.');
         }
     };
+    
 
     const handleUpload = async () => {
         if (selectedFile) {
@@ -27,14 +46,14 @@ const Upimg = () => {
             formData.append('file', selectedFile);
 
             try {
-                const response = await axios.post(`http://${serverIp}:${serverPort}/in/upimg`, formData, {
+                const response = await axios.post(`http://${serverIp}:${serverPort}/in/img`, formData, {
                     headers: {
                         'Content-Type': 'multipart/form-data',
                     },
                 });
                 console.log('서버 응답:', response.data);
             } catch (error) {
-                console.error('업로드 실패:', error);
+                console.log(error.response.data)
             }
         }
     };
